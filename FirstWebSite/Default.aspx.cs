9278f2f9a0e2849164e56b1fb214b8024a6b1575
Lomar Lilly <lithiumlilly3@gmail.com>
2017-09-13 13:03:05 -0500

    Week 3 Modifications
    Capture student data and add to Student information class on button click event.

--- FirstWebSite/Default.aspx.cs
+++ FirstWebSite/Default.aspx.cs
@@ -11,5 +11,18 @@ public partial class _Default : System.Web.UI.Page
     {
 
     }
+   
+    protected void submitSudentInformation_Click(object sender, EventArgs e)
+    {
+        StudentInformation student = new StudentInformation();
 
+        student.StudentId = Convert.ToInt32(txtStudentID.Text);
+        student.FirstName = txtFirstName.Text;
+        student.LastName = txtLastName.Text;
+        student.Gender = rblGender.SelectedItem.Text;
+        student.DateOfBirth = calDateOfBirth.SelectedDate;
+        student.ContactNumber = txtContactNumber.Text;
+        student.Faculty = ddlFaculty.SelectedItem.Text;
+        student.CourseOfStudy = ddlCourseOfStudy.SelectedItem.Text;
+    }
 }
